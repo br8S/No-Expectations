68610dff7957ab0e39f170d35ab326d8af8c0260
br8S <56180503+br8S@users.noreply.github.com>
2021-03-24 17:20:36 -0700

    added soundcloud links to mix images

--- imageHover.js
+++ imageHover.js
@@ -1,37 +1,52 @@
+/* IMAGE 5 EVENT LISTENERS */
+
 document.querySelector(".img-5").addEventListener('mouseover', () => {
     document.querySelector(".img-5").src = "images/artist-005-320p.jpg";
+    document.querySelector(".img-5").style.cursor = "pointer";
 })
 
 document.querySelector(".img-5").addEventListener('mouseleave', () => {
     document.querySelector(".img-5").src = "images/mix-005-320p.jpg";
 })
 
+/* IMAGE 4 EVENT LISTENERS */
+
 document.querySelector(".img-4").addEventListener('mouseover', () => {
     document.querySelector(".img-4").src = "images/artist-004-320p.jpg";
+    document.querySelector(".img-4").style.cursor = "pointer";
 })
 
 document.querySelector(".img-4").addEventListener('mouseleave', () => {
     document.querySelector(".img-4").src = "images/mix-004-320p.jpg";
 })
 
+/* IMAGE 3 EVENT LISTENERS */
+
 document.querySelector(".img-3").addEventListener('mouseover', () => {
     document.querySelector(".img-3").src = "images/artist-003-320p.jpg";
+    document.querySelector(".img-3").style.cursor = "pointer";
 })
 
 document.querySelector(".img-3").addEventListener('mouseleave', () => {
     document.querySelector(".img-3").src = "images/mix-003-320p.jpg";
 })
 
+/* IMAGE 2 EVENT LISTENERS */
+
 document.querySelector(".img-2").addEventListener('mouseover', () => {
     document.querySelector(".img-2").src = "images/artist-002-320p.jpg";
+    document.querySelector(".img-2").style.cursor = "pointer";
 })
 
 document.querySelector(".img-2").addEventListener('mouseleave', () => {
     document.querySelector(".img-2").src = "images/mix-002-320p.jpg";
 })
 
+/* IMAGE 1 EVENT LISTENERS */
+
 document.querySelector(".img-1").addEventListener('mouseover', () => {
     document.querySelector(".img-1").src = "images/artist-001-320p.jpg";
+    document.querySelector(".img-1").style.cursor = "pointer";
 })
 
 document.querySelector(".img-1").addEventListener('mouseleave', () => {
@@ -51,7 +66,7 @@ for(var i=0; i < mix_images.length; i++){
         })
 
         mix_images[i].addEventListener('mouseleave', () => {
-            mix_images[i].style.opacity = "0.8";
+            mix_images[i].style.opacity = "0.5";
             console.log
         })
     }(i));
