c60751c8630e5df7c5498004afb8c76a9b80d93a
br8S <56180503+br8S@users.noreply.github.com>
2021-03-25 21:35:13 -0700

    added different fill to mute botton

--- music.js
+++ music.js
@@ -3,10 +3,12 @@ var musicOn = true;
 document.querySelector('.fa-volume-mute').addEventListener('click', () => {
     if(musicOn === true) {
         document.querySelector('.song').pause();
+        document.querySelector('.mutePath').style.fill = "#c5ff29";
         musicOn = false;
     }
     else{
         document.querySelector('.song').play();
+        document.querySelector('.mutePath').style.fill = "gainsboro";
         musicOn = true;
     }
 })
